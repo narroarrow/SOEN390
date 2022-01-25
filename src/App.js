558a10119ae5db5ead558a9f218c29c6034bdc55
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2022-01-24 19:06:10 -0500

    Commit Fix for Frontend Git ignore

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Header from './components/Header'
+
+function App() {
+  return (
+
+    <div className="container">
+      <Header title="Covid 19 Apps"/>
+    </div>
+  );
+}
+
+export default App;
