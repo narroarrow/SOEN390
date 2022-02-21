bbc1a5498ab3b6bd96014a2841fbc2dce28ddeff
alexbara2000 <60298505+alexbara2000@users.noreply.github.com>
2022-02-21 12:21:09 -0500

    fixed manifesto issues #79

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 const theme = createTheme(
 {
   palette: {
-    type: 'light',
+    mode: 'light',
     primary: {
       main: '#26495c',
       contrastText: '#ffffff',
