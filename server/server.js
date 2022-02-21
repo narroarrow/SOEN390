abc259f500b680dcc8dca84b648b3c4c56d2a7bd
JeffreyCHChan <chan.jch19@gmail.com>
2022-02-21 10:25:36 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.get('/api', (req, res) => {
 
 
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
 })
 
 
