d0fee72988a06131038547084b47091b40e7a100
JeffreyCHChan <chan.jch19@gmail.com>
2022-02-28 14:55:15 -0500

    auto assign doctor to a patient

--- server/signup.js
+++ server/signup.js
@@ -95,7 +95,7 @@ app.post("/Signup", async(req,res) => {
         let password = req.body.password;
         let userRole = req.body.userRole
         let phoneNumber = req.body.phoneNumber
-
+        const uid = Math.floor(Math.random()*100000)
         const salt = await bcrypt.genSalt(10)//hashes with 10 rounds
         const hashedPassword = await bcrypt.hash(password,salt)
 
@@ -108,13 +108,44 @@ app.post("/Signup", async(req,res) => {
             Validated = 1;
         }
         console.log(userRole)
-        db.query(state, [Math.floor(Math.random()*100000),firstName,lastName,email,hashedPassword,Validated,phoneNumber,userRole], function(err, result) {//ID might be removed since it should be auto indent
+        
+        db.query(state, [uid,firstName,lastName,email,hashedPassword,Validated,phoneNumber,userRole], function(err, result) {//ID might be removed since it should be auto indent
             if(err){
                 console.log(err)}
             else{
-                res.send(result);
+                
                 }
         })
+
+
+
+
+        if(userRole == 'Patient'){
+
+            state = `SELECT p.DoctorID FROM 390db.patients p Group By P.DoctorID order by Count(p.ID) asc Limit 1;`;
+            db.query(state, function(err, result) {//finds the doctor with the least amount of patients 
+                if(err){
+                    console.log(err)}
+                else{
+                    let docID = result[0].DoctorID 
+                    // console.log("DoctorID:\t\t"+docID)
+
+
+                    // console.log("userID:\t\t"+ uid)
+                    patientState = `INSERT INTO 390db.patients (ID, DoctorID, Flagged) VALUES (?,?,?);`;
+                    db.query(patientState, [uid,docID,0], function(err, result) {//inserts a new patient with an auto assigned doctor
+                        if(err){
+                            console.log("\ninserting into patient \n"+err)}
+                        else{
+                        }
+                    })
+                    }
+            })
+            
+
+        }
+// final send
+        res.send(result);
     }
     catch{ 
         res.status(500).send()
