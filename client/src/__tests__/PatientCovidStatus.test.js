02cf18cb594b16e11427fa47ee40ea6e55de59be
Michael Warner <michael.gregory.warner@gmail.com>
2022-02-28 12:12:28 -0500

    added tests for each page

---
+++ client/src/__tests__/PatientCovidStatus.test.js
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+import PatientCovidStatus from './pages/PatientCovidStatus';
+
+
+test('Renders the PatientCovidStatus component', () => {
+  render(<PatientCovidStatus />);
+});
