6ce8d97bb7577565ee88802ed8c2594f3287ba29
alexbara2000 <60298505+alexbara2000@users.noreply.github.com>
2022-03-01 12:34:22 -0500

    Fixed unit tests

--- client/src/__tests__/HealthOfficialPatientProfile.test.js
+++ client/src/__tests__/HealthOfficialPatientProfile.test.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import HealthOfficialPatientProfile from '../pages/HealthOfficialPatientProfile';
 
-test('Renders the DoctorViewingPatient page', () => {
+test('Renders the HealthOfficialPatientProfile page', () => {
     render(<HealthOfficialPatientProfile />);
   });
