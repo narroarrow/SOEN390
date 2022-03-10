813083984c28b7007ba736a9c91107c63f24ed55
Michael Warner <michael.gregory.warner@gmail.com>
2022-03-09 21:33:38 -0500

    init commit for doctorScheduler

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,7 @@ import HealthOfficialPatientProfile from './pages/HealthOfficialPatientProfile.j
 import HealthOfficialViewingPatient from './pages/HealthOfficialViewingPatient.js';
 import EditInfoForm from './pages/EditInfoForm';
 import PatientCovidStatus from './pages/PatientCovidStatus';
+import DoctorSchedule from './pages/DoctorSchedule';
 
 
 function App() {
@@ -81,6 +82,7 @@ function App() {
           <Route path='/EditInfoForm' element={<EditInfoForm/>}/>
           <Route path='/PatientCovidStatus' element = {<PatientCovidStatus/>}/>
           <Route path='/SymptomForm' element={<SymptomForm/>}/>
+          <Route path='/DoctorSchedule' element={<DoctorSchedule/>}/>
           
         </Routes>
         <Footer/>
