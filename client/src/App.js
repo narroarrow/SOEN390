d8652193cc6155582769c9315c7af205ffa732c6
eric <erichanna2009@hotmail.com>
2022-03-10 19:27:39 -0500

    Rerender navbar, login and signup functionalities, correct patient prof

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Signup from './pages/Signup.js';
 import AdminDashboard from './pages/AdminDashboard';
 import SymptomForm from './pages/SymptomForm';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Switch, Route, Routes } from 'react-router-dom';
+import {Switch, Route, Routes} from 'react-router-dom';
 import Footer from './components/Footer.js';
 import Navbar from './components/Navbar.js';
 import DoctorDashboard from './pages/DoctorDashboard.js';
@@ -16,76 +16,62 @@ import HealthOfficialPatientProfile from './pages/HealthOfficialPatientProfile.j
 import HealthOfficialViewingPatient from './pages/HealthOfficialViewingPatient.js';
 import EditInfoForm from './pages/EditInfoForm';
 import PatientCovidStatus from './pages/PatientCovidStatus';
-
+import Cookies from 'js-cookie';
+import Common from './pages/Common'
 
 function App() {
 
     const [exampleData, setExampleData] = useState([{}])
 
-    // one test of calling to backend
-    useEffect(() => {
-
-        const callCheckHealth = async () => {
-
-            try {
-
-                const url = "http://localhost:8080/api"
-                const headers = {
-                    "Accept": "application/json",
-                    "Content-Type": "application/json"
-                }
-                const res = await axios(url);
-                console.log("Data from backend---", res.data)
-                setExampleData(res.data)
-                console.log('the example data is: ', res.data)
 
 
-            } catch (err) {
-                console.log(err);
-            }
-        }
-        callCheckHealth();
-    }, [])
-
-    //second test using cookies
-
-    function secondTest() {
-        axios.get(
-            "http://localhost:8080/users", {withCredentials: true}).then(res => {
-            console.log(res)
-        }).catch(err => console.log(err))
-    }
 
     function checkAuth() {
-        axios.get(
-            "http://localhost:8080/checkAuth", {withCredentials: true}).then(res => console.log(res)
-            // USER DATA IS IN RES
-        ).catch(err => console.log(err))
-    }
-    useEffect(() => {checkAuth();}, [])
-  return (
-      <div className="App">
-        <CssBaseline />
+        return new Promise(((resolve, reject) => {
+            axios.get(
+                "http://localhost:8080/checkAuth", {withCredentials: true}).then(res => {
 
-        <Navbar/>
-        <Routes>
-          <Route path='/login' element={<Login/>} />
-          <Route path='/Signup' element={<Signup/>} />
-          <Route path='/DoctorDashboard' element={<DoctorDashboard/>} />
-          <Route path='/DoctorPatientProfile' element={<DoctorPatientProfile/>} />
-          <Route path='/AdminDashboard' element={<AdminDashboard/>}/>
-          <Route path='/PatientProfile' element={<PatientProfile/>}/>
-          <Route path='/DoctorViewingPatient' element={<DoctorViewingPatient/>}/>
-          <Route path='/HealthOfficialPatientProfile' element={<HealthOfficialPatientProfile/>}></Route>
-          <Route path='/HealthOfficialViewingPatient' element={<HealthOfficialViewingPatient/>}></Route>
-          <Route path='/EditInfoForm' element={<EditInfoForm/>}/>
-          <Route path='/PatientCovidStatus' element = {<PatientCovidStatus/>}/>
-          <Route path='/SymptomForm' element={<SymptomForm/>}/>
-          
-        </Routes>
-        <Footer/>
-      </div>
-  );
+                    localStorage.setItem("role", res.data.role)
+                    localStorage.setItem("id", res.data.id)
+                    resolve(res.data);
+                }
+
+            ).catch(err => {
+                console.log(err)
+            })
+        }))
+    }
+
+    useEffect(() => {
+        checkAuth();
+    }, [])
+    return (
+        <div className="App">
+            <CssBaseline/>
+            <Common.Provider value={{checkAuth}}>
+
+                <Navbar/>
+                <Routes>
+
+                    <Route path='/login' element={<Login/>} checkAuth={checkAuth()}/>
+                    <Route path='/Signup' element={<Signup/>}/>
+                    <Route path='/DoctorDashboard' element={<DoctorDashboard/>}/>
+                    <Route path='/DoctorPatientProfile' element={<DoctorPatientProfile/>}/>
+                    <Route path='/AdminDashboard' element={<AdminDashboard/>}/>
+                    <Route path='/PatientProfile' element={<PatientProfile/>}/>
+                    <Route path='/DoctorViewingPatient' element={<DoctorViewingPatient/>}/>
+                    <Route path='/HealthOfficialPatientProfile' element={<HealthOfficialPatientProfile/>}></Route>
+                    <Route path='/HealthOfficialViewingPatient' element={<HealthOfficialViewingPatient/>}></Route>
+                    <Route path='/EditInfoForm' element={<EditInfoForm/>}/>
+                    <Route path='/PatientCovidStatus' element={<PatientCovidStatus/>}/>
+                    <Route path='/SymptomForm' element={<SymptomForm/>}/>
+
+
+                </Routes>
+            </Common.Provider>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,88 +1,173 @@
 import * as React from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
-import {Container, Box, Button, AppBar, Toolbar, IconButton, Typography, Menu, Avatar, Tooltip, MenuItem} from '@mui/material';
+import {
+    Container,
+    Box,
+    Button,
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+    Menu,
+    Avatar,
+    Tooltip,
+    MenuItem
+} from '@mui/material';
+import axios from "axios";
+import {useEffect} from "react";
+import Cookies from 'js-cookie';
+import Axios from "axios";
+import Common from "../pages/Common";
+import Login from "../pages/Login";
 
-const pages = ['Login', 'Signup', 'AdminDashboard', 'DoctorPatientProfile', 'DoctorDashboard', 'PatientProfile'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+let pages = [];
+let settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
 
 const ResponsiveAppBar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [user, setUser] = React.useState(null);
+    const [pagesTest, setPagesTest] = React.useState([]);
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    function instantiateNavBar() {
+        {
 
-  return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }} >
-            COVID-19 App
-          </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
-              <MenuIcon />
-            </IconButton>
-            <Menu id="menu-appbar" anchorEl={anchorElNav}
-              anchorOrigin={{ vertical: 'bottom', horizontal: 'left',}}
-              keepMounted
-              transformOrigin={{ vertical: 'top', horizontal: 'left',}}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{display: { xs: 'block', md: 'none' },}}>
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-            
-          </Box>
-          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            COVID-19 App
-          </Typography>
+            setPagesTest(pages);
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button key={page} onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }} href = {`/${page}`}>
-                {page}
-              </Button>
-            ))}
-          </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu sx={{ mt: '45px' }} id="menu-appbar" anchorEl={anchorElUser} anchorOrigin={{ vertical: 'top', horizontal: 'right',}} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right',}}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+            if (localStorage.getItem('role') == null) {
+                pages.push('Login');
+                pages.push('Signup')
+            }
+            if (localStorage.getItem('role') == 'Patient') {
+                pages.push('PatientProfile');
+            }
+            if (localStorage.getItem('role') == 'Admin') {
+                pages.push('AdminDashboard');
+                pages.push('DoctorPatientProfile');
+            }
+            if (localStorage.getItem('role') == 'Doctor') {
+                pages.push('DoctorDashboard');
+                pages.push('DoctorPatientProfile');
+            }
+            if (localStorage.getItem('role') != null) {
+                pages.push('Logout');
+            }
+
+
+        }
+    }
+
+    function logout() {
+        Axios.post('http://localhost:8080/Logout', {}, {withCredentials: true}).then(() => {
+            localStorage.clear();
+
+
+            return new Promise(((resolve, reject) => {
+                axios.get(
+                    "http://localhost:8080/checkAuth", {withCredentials: true}).catch(err => {
+                    window.location.reload();
+
+                })
+            }))
+
+
+        });
+    }
+
+    useEffect(() => {
+        instantiateNavBar();
+    }, [])
+
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography variant="h6" noWrap component="div" sx={{mr: 2, display: {xs: 'none', md: 'flex'}}}>
+                        COVID-19 App
+                    </Typography>
+
+                    <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                        <IconButton size="large" aria-label="account of current user" aria-controls="menu-appbar"
+                                    aria-haspopup="true" onClick={handleOpenNavMenu} color="inherit">
+                            <MenuIcon/>
+                        </IconButton>
+                        <Menu id="menu-appbar" anchorEl={anchorElNav}
+                              anchorOrigin={{vertical: 'bottom', horizontal: 'left',}}
+                              keepMounted
+                              transformOrigin={{vertical: 'top', horizontal: 'left',}}
+                              open={Boolean(anchorElNav)}
+                              onClose={handleCloseNavMenu}
+                              sx={{display: {xs: 'block', md: 'none'},}}>
+                            {pagesTest.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+
+                    </Box>
+                    <Typography variant="h6" noWrap component="div"
+                                sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                        COVID-19 App
+                    </Typography>
+
+                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                        {pages.map((page) => (
+                            page === 'Logout' ? <Button key={page} onClick={logout}
+                                                        sx={{my: 2, color: 'white', display: 'block'}}>
+                                    {page}
+                                </Button> :
+
+                                <Button key={page} onClick={handleCloseNavMenu}
+                                        sx={{my: 2, color: 'white', display: 'block'}} href={`/${page}`}>
+                                    {page}
+                                </Button>
+                        ))}
+                    </Box>
+                    <Box sx={{flexGrow: 0}}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                                {localStorage.getItem('role') && <Avatar alt="Remy Sharp"
+                                                                         src="https://icon-library.com/images/avatar-icon-images/avatar-icon-images-4.jpg"/>
+                                }             </IconButton>
+                        </Tooltip>
+                        <Menu sx={{mt: '45px'}} id="menu-appbar" anchorEl={anchorElUser}
+                              anchorOrigin={{vertical: 'top', horizontal: 'right',}} keepMounted
+                              transformOrigin={{vertical: 'top', horizontal: 'right',}}
+                              open={Boolean(anchorElUser)}
+                              onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={setting === 'Logout' ? logout : handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 };
+
+ResponsiveAppBar.contextType = Common;
+
 export default ResponsiveAppBar;
