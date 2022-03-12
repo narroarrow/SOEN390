8e633a34dab05f438ef787d0181928f2e4ba430f
eric <erichanna2009@hotmail.com>
2022-03-12 14:31:14 -0500

    getting and sending data placeholder method

--- client/src/pages/DoctorSchedule.js
+++ client/src/pages/DoctorSchedule.js
@@ -1,100 +1,120 @@
-import { Paper, FormControlLabel, FormControl, FormLabel, Checkbox, FormGroup, Button} from '@mui/material'; 
-import React, { useState, useEffect } from "react";
+import {Paper, FormControlLabel, FormControl, FormLabel, Checkbox, FormGroup, Button} from '@mui/material';
+import React, {useState, useEffect} from "react";
 import * as moment from "moment";
+import Axios from "axios";
 
 const TimeSlotDayTable = (props) => {
-  // Object destructuring, get the day and timeSlots properties from props, which we passed in when we used .map in the TimeSlotCalendar component
-  const { day, slots, handleChange } = props;
+    // Object destructuring, get the day and timeSlots properties from props, which we passed in when we used .map in the TimeSlotCalendar component
+    const {day, slots, handleChange} = props;
 
-  return (
-    
-    <div className="time-slot-day">
-      <h1>{day}</h1>
+    return (
 
-      <div className="time-slot-intervals">
-        {slots.map((timeSlot) => (
-          <FormControlLabel key={`${day} - ${timeSlot.label}`} control={<Checkbox />} onChange={(event) => handleChange(event, timeSlot)} label={timeSlot.label} />
-        ))}
-      </div>
-    </div>
-    
-    
-  );
+        <div className="time-slot-day">
+            <h1>{day}</h1>
+
+            <div className="time-slot-intervals">
+                {slots.map((timeSlot) => (
+                    <FormControlLabel key={`${day} - ${timeSlot.label}`} control={<Checkbox/>}
+                                      onChange={(event) => handleChange(event, timeSlot)} label={timeSlot.label}/>
+                ))}
+            </div>
+        </div>
+
+
+    );
 };
 
 const TimeSlotCalendar = () => {
-  const DAYS_TO_DISPLAY = 5; // update this to change how many days you show automatically
-  const TIME_SLOT_INTERVAL_IN_MINUTES = 30;
-  const currentDate = moment().isoWeekday(0).startOf("day");
-  const [timeSlotsPerDay, setTimeSlotsPerDay] = useState([]);
-  const selectedTimeSlots = [];
+    const DAYS_TO_DISPLAY = 5; // update this to change how many days you show automatically
+    const TIME_SLOT_INTERVAL_IN_MINUTES = 30;
+    const currentDate = moment().isoWeekday(0).startOf("day");
+    const [timeSlotsPerDay, setTimeSlotsPerDay] = useState([]);
+    const selectedTimeSlots = [];
 
-  const handleChange = (event, timeSlot) => {
-    if (event.target.checked) {
-      selectedTimeSlots.push(timeSlot);
-    } else {
-      const indexToRemove = selectedTimeSlots.indexOf(timeSlot);
-      if (indexToRemove > -1) {
-        selectedTimeSlots.splice(indexToRemove, 1); // 2nd parameter means remove one item only
-      }
-    }
-  };
+    const handleChange = (event, timeSlot) => {
 
-  const calculateTimeSlots = (startHour, endHour) => {
-    const calculatedTimeSlots = [];
-    // 17 - 8 = 9, 9 / 0.5 = 18 - 2 = 16, since you don't want to count 8 and 17 hours, but double check with other calculations
-    const numberOfTimeSlots = (endHour - startHour) / (TIME_SLOT_INTERVAL_IN_MINUTES / 60);
-    for (var i = 0; i < DAYS_TO_DISPLAY; i++) {
-      let day = currentDate.add(1, "days");
+        if (event.target.checked) {
+            selectedTimeSlots.push(timeSlot);
+        } else {
+            const indexToRemove = selectedTimeSlots.indexOf(timeSlot);
+            if (indexToRemove > -1) {
+                selectedTimeSlots.splice(indexToRemove, 1); // 2nd parameter means remove one item only
+            }
+        }
+    };
+
+    const calculateTimeSlots = (startHour, endHour) => {
+        const calculatedTimeSlots = [];
+        // 17 - 8 = 9, 9 / 0.5 = 18 - 2 = 16, since you don't want to count 8 and 17 hours, but double check with other calculations
+        const numberOfTimeSlots = (endHour - startHour) / (TIME_SLOT_INTERVAL_IN_MINUTES / 60);
+        for (var i = 0; i < DAYS_TO_DISPLAY; i++) {
+            let day = currentDate.add(1, "days");
 
 
-      
-      
-      let timeSlots = {
-        day: day.format("dddd"),
-        slots: [],
-      };
+            let timeSlots = {
+                day: day.format("dddd"),
+                slots: [],
+            };
 
-      // the date will be Date-00-00-00, whatever, midnight, so add enough hours to get to your start time, so 8 am
-      let incrementedTime = moment(day).add(startHour, "h");
 
-      for (var j = 0; j < numberOfTimeSlots; j++) {
-        let intervalEnd = moment(incrementedTime).add(TIME_SLOT_INTERVAL_IN_MINUTES, "m");
-  
-        timeSlots.slots.push({
-          label: `${incrementedTime.format("hh:mm")} - ${intervalEnd.format("hh:mm")}`,
-          day: `${day.format("dddd")}`,
-          startTime: incrementedTime,
-          endTime: intervalEnd,
+            // the date will be Date-00-00-00, whatever, midnight, so add enough hours to get to your start time, so 8 am
+            let incrementedTime = moment(day).add(startHour, "h");
+
+            for (var j = 0; j < numberOfTimeSlots; j++) {
+                let intervalEnd = moment(incrementedTime).add(TIME_SLOT_INTERVAL_IN_MINUTES, "m");
+
+                timeSlots.slots.push({
+                    label: `${incrementedTime.format("hh:mm")} - ${intervalEnd.format("hh:mm")}`,
+                    day: `${day.format("dddd")}`,
+                    startTime: incrementedTime,
+                    endTime: intervalEnd,
+                });
+
+                incrementedTime.add(TIME_SLOT_INTERVAL_IN_MINUTES, "m");
+            }
+
+            calculatedTimeSlots.push(timeSlots);
+        }
+        setTimeSlotsPerDay(calculatedTimeSlots);
+    };
+    const submit = () => {
+        selectedTimeSlots.forEach(timeSlot => {
+
+        })
+        let backendTimeSlots = [];
+        selectedTimeSlots.forEach(timeSlot => {
+            backendTimeSlots.push({
+                day: timeSlot.day.substr(0, 3),
+                doctorID: 1,
+                startTime: timeSlot.label.substr(0, 5).concat(":00"),
+                endTime: timeSlot.label.substr(8, 14).concat(":00")
+            })
+        })
+
+        let backendData = {backendTimeSlots}
+
+        Axios.post('http://localhost:8080/doctorAppointments(JEFF-METHOD-NAME)', backendData, {withCredentials: true}).then(res => {
+            console.log(res)
+        }).catch((err) => {
+            console.log(err)
         });
 
-        incrementedTime.add(TIME_SLOT_INTERVAL_IN_MINUTES, "m");
-      }
-
-      calculatedTimeSlots.push(timeSlots);
     }
+    useEffect(() => {
+        calculateTimeSlots(8, 17);
+    }, []);
 
-    setTimeSlotsPerDay(calculatedTimeSlots);
-  };
+    return <div>
+        {timeSlotsPerDay.length > 0 && timeSlotsPerDay.map((timeSlotsOnDay, index) => <TimeSlotDayTable
+            handleChange={handleChange} key={`${index}`} day={timeSlotsOnDay.day} slots={timeSlotsOnDay.slots}/>)}
 
-  useEffect(() => {
-    calculateTimeSlots(8, 17);
-  }, []);
-
-  return <div>
-    {timeSlotsPerDay.length > 0 && timeSlotsPerDay.map((timeSlotsOnDay, index) => <TimeSlotDayTable handleChange={handleChange} key={`${index}`} day={timeSlotsOnDay.day} slots={timeSlotsOnDay.slots} />)}
-    
-    <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
-          Submit
+        <Button type="submit" variant="contained" sx={{mt: 3, mb: 2}} onClick={submit}>
+            Submit
         </Button>
     </div>;
 };
 
 export default TimeSlotCalendar;
-
-
-
-
 
 
 // currentDate = new Date();
@@ -103,7 +123,6 @@ export default TimeSlotCalendar;
 // for (var i = 0; i < 5; i++) {
 // days.push(new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay() + i)).toDateString());
 // }
-
 
 
 // const DAYS_TO_DISPLAY = 5; // update this to change how many days you show automatically
@@ -123,7 +142,7 @@ export default TimeSlotCalendar;
 
 //     // the date will be Date-00-00-00, whatever, midnight, so add enough hours to get to your start time, so 8 am
 //     let incrementedTime = day.AddHours(startHour);
-    
+
 //     for (var j = 0; j < timeSlots ; j++) {
 //         // format the getTime, I don't remember what the function is for it to do that, you'll have to look it up
 //         timeSlots.slots.push({ 
@@ -138,7 +157,6 @@ export default TimeSlotCalendar;
 // }
 
 
-
 // export default function DoctorSchedule(){
 
 //         return (
@@ -148,10 +166,10 @@ export default TimeSlotCalendar;
 //             <h1>Doctor Schedule</h1>
 
 //             <Box sx = {{ display: 'flex', justifyContent: 'center'}}>
-        
+
 //             <FormGroup sx={{ px:5,  border: 1}}>
 //             <FormLabel component="legend">Monday</FormLabel>
-               
+
 //                 <FormControlLabel control={<Checkbox/>} label="8:00 - 8:30" />
 //                 <FormControlLabel control={<Checkbox/>} label="8:30 - 9:00" />
 //                 <FormControlLabel control={<Checkbox/>} label="9:00 - 9:30"/>
@@ -168,8 +186,8 @@ export default TimeSlotCalendar;
 //                 <FormControlLabel control={<Checkbox/>} label="15:30 - 16:00" />
 //                 <FormControlLabel control={<Checkbox/>} label="16:00 - 16:30"/>
 //                 <FormControlLabel control={<Checkbox/>} label="16:30 - 17:00" />
-               
-                
+
+
 //             </FormGroup>
 
 //             <FormGroup sx={{ px:5, textAlign:"center"}}>
@@ -190,7 +208,7 @@ export default TimeSlotCalendar;
 //                 <FormControlLabel control={<Checkbox/>} label="15:30 - 16:00" />
 //                 <FormControlLabel control={<Checkbox/>} label="16:00 - 16:30"/>
 //                 <FormControlLabel control={<Checkbox/>} label="16:30 - 17:00" />
-                
+
 //             </FormGroup>
 
 //             <FormGroup sx={{ px:5}}>
@@ -211,7 +229,7 @@ export default TimeSlotCalendar;
 //                 <FormControlLabel control={<Checkbox/>} label="15:30 - 16:00" />
 //                 <FormControlLabel control={<Checkbox/>} label="16:00 - 16:30"/>
 //                 <FormControlLabel control={<Checkbox/>} label="16:30 - 17:00" />
-                
+
 //             </FormGroup>
 
 //             <FormGroup sx={{ px:5}}>
@@ -232,7 +250,7 @@ export default TimeSlotCalendar;
 //                 <FormControlLabel control={<Checkbox/>} label="15:30 - 16:00" />
 //                 <FormControlLabel control={<Checkbox/>} label="16:00 - 16:30"/>
 //                 <FormControlLabel control={<Checkbox/>} label="16:30 - 17:00" />
-                
+
 //             </FormGroup>
 
 //             <FormGroup sx={{ px:5}}>
@@ -253,11 +271,11 @@ export default TimeSlotCalendar;
 //                 <FormControlLabel control={<Checkbox/>} label="15:30 - 16:00" />
 //                 <FormControlLabel control={<Checkbox/>} label="16:00 - 16:30"/>
 //                 <FormControlLabel control={<Checkbox/>} label="16:30 - 17:00" />
-                
+
 //             </FormGroup>
-          
+
 //             </Box>
-            
+
 //             <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
 //           Submit
 //         </Button>
