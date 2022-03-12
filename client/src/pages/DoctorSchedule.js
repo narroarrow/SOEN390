e4dedd087e715e2f4f42ac7dc91b87e3a7cdc4ad
Michael Warner <michael.gregory.warner@gmail.com>
2022-03-12 14:42:54 -0500

    updated doctor schedule

--- client/src/pages/DoctorSchedule.js
+++ client/src/pages/DoctorSchedule.js
@@ -1,7 +1,8 @@
-import {Paper, FormControlLabel, FormControl, FormLabel, Checkbox, FormGroup, Button} from '@mui/material';
+import {Paper, FormControlLabel, FormControl, FormLabel, Checkbox, Button} from '@mui/material';
 import React, {useState, useEffect} from "react";
 import * as moment from "moment";
 import Axios from "axios";
+import { Box } from '@mui/system';
 
 const TimeSlotDayTable = (props) => {
     // Object destructuring, get the day and timeSlots properties from props, which we passed in when we used .map in the TimeSlotCalendar component
@@ -45,7 +46,7 @@ const TimeSlotCalendar = () => {
 
     const calculateTimeSlots = (startHour, endHour) => {
         const calculatedTimeSlots = [];
-        // 17 - 8 = 9, 9 / 0.5 = 18 - 2 = 16, since you don't want to count 8 and 17 hours, but double check with other calculations
+        // 17 - 8 = 9, 9 / 0.5 = 18 - 2 = 16, since you don't want to count 8 and 17 hours,
         const numberOfTimeSlots = (endHour - startHour) / (TIME_SLOT_INTERVAL_IN_MINUTES / 60);
         for (var i = 0; i < DAYS_TO_DISPLAY; i++) {
             let day = currentDate.add(1, "days");
@@ -89,9 +90,9 @@ const TimeSlotCalendar = () => {
             })
         })
 
-        let backendData = {backendTimeSlots}
+        let doctorScheduleData = {backendTimeSlots}
 
-        Axios.post('http://localhost:8080/doctorAppointments(JEFF-METHOD-NAME)', backendData, {withCredentials: true}).then(res => {
+        Axios.post('http://localhost:8080/doctorAppointments(JEFF-METHOD-NAME)', doctorScheduleData, {withCredentials: true}).then(res => {
             console.log(res)
         }).catch((err) => {
             console.log(err)
@@ -102,185 +103,14 @@ const TimeSlotCalendar = () => {
         calculateTimeSlots(8, 17);
     }, []);
 
-    return <div>
+    return <Box sx={{ p: 10 }}>
         {timeSlotsPerDay.length > 0 && timeSlotsPerDay.map((timeSlotsOnDay, index) => <TimeSlotDayTable
             handleChange={handleChange} key={`${index}`} day={timeSlotsOnDay.day} slots={timeSlotsOnDay.slots}/>)}
 
         <Button type="submit" variant="contained" sx={{mt: 3, mb: 2}} onClick={submit}>
             Submit
         </Button>
-    </div>;
+    </Box>;
 };
 
 export default TimeSlotCalendar;
-
-
-// currentDate = new Date();
-// days = [];
-
-// for (var i = 0; i < 5; i++) {
-// days.push(new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay() + i)).toDateString());
-// }
-
-
-// const DAYS_TO_DISPLAY = 5; // update this to change how many days you show automatically
-// const TIME_SLOT_INTERVAL_IN_MINUTES = 30; 
-// const currentDate = new Date();
-// const timeSlotsPerDay = [];
-
-// const CalculateTimeSlots = (startHour, endHour) => {
-// // 17 - 8 = 9, 9 / 0.5 = 18 - 2 = 16, since you don't want to count 8 and 17 hours, but double check with other calculations
-// const timeSlots = ((endHour - startHour) / (60 / TIME_SLOT_INTERVAL_IN_MINUTES)) - 2;
-// for (var i = 0; i < DAYS_TO_DISPLAY ; i++) {
-//     day = currentDate.AddDays(i);
-//     let timeSlots = {
-//         day: day.getDate, // look up how to get the day's name from the date
-//         slots: [],
-//     }
-
-//     // the date will be Date-00-00-00, whatever, midnight, so add enough hours to get to your start time, so 8 am
-//     let incrementedTime = day.AddHours(startHour);
-
-//     for (var j = 0; j < timeSlots ; j++) {
-//         // format the getTime, I don't remember what the function is for it to do that, you'll have to look it up
-//         timeSlots.slots.push({ 
-//             label: `${incrementedTime.getTime} - ${incrementedTime.addMinuts(TIME_SLOT_INTERVAL_IN_MINUTES).getTime}`, 
-//             startTime : incrementedTime.getTime,
-//             endTime : incrementedTime.addMinuts(TIME_SLOT_INTERVAL_IN_MINUTES).getTime
-//         });
-
-//         incrementedTime = incrementedTime.addMinutes(TIME_SLOT_INTERVAL_IN_MINUTES).getTime;
-//     }
-// }
-// }
-
-
-// export default function DoctorSchedule(){
-
-//         return (
-
-//             <div align="Center">
-//             <Paper elevation={24} component="form" sx={{ width: 1400, height: 1000, mt: 10}}>
-//             <h1>Doctor Schedule</h1>
-
-//             <Box sx = {{ display: 'flex', justifyContent: 'center'}}>
-
-//             <FormGroup sx={{ px:5,  border: 1}}>
-//             <FormLabel component="legend">Monday</FormLabel>
-
-//                 <FormControlLabel control={<Checkbox/>} label="8:00 - 8:30" />
-//                 <FormControlLabel control={<Checkbox/>} label="8:30 - 9:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="9:00 - 9:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="9:30 - 10:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="10:00 - 10:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="10:30 - 11:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="11:00 - 11:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="11:30 - 12:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="12:00 - 12:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="12:30 - 13:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="13:00 - 13:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="13:30 - 14:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="14:00 - 14:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="15:30 - 16:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="16:00 - 16:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="16:30 - 17:00" />
-
-
-//             </FormGroup>
-
-//             <FormGroup sx={{ px:5, textAlign:"center"}}>
-//             <FormLabel component="legend">Tuesday</FormLabel>
-//             <FormControlLabel control={<Checkbox/>} label="8:00 - 8:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="8:30 - 9:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="9:00 - 9:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="9:30 - 10:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="10:00 - 10:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="10:30 - 11:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="11:00 - 11:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="11:30 - 12:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="12:00 - 12:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="12:30 - 13:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="13:00 - 13:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="13:30 - 14:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="14:00 - 14:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="15:30 - 16:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="16:00 - 16:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="16:30 - 17:00" />
-
-//             </FormGroup>
-
-//             <FormGroup sx={{ px:5}}>
-//             <FormLabel component="legend">Wednesday</FormLabel>
-//             <FormControlLabel control={<Checkbox/>} label="8:00 - 8:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="8:30 - 9:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="9:00 - 9:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="9:30 - 10:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="10:00 - 10:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="10:30 - 11:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="11:00 - 11:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="11:30 - 12:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="12:00 - 12:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="12:30 - 13:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="13:00 - 13:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="13:30 - 14:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="14:00 - 14:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="15:30 - 16:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="16:00 - 16:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="16:30 - 17:00" />
-
-//             </FormGroup>
-
-//             <FormGroup sx={{ px:5}}>
-//             <FormLabel component="legend">Thursday</FormLabel>
-//             <FormControlLabel control={<Checkbox/>} label="8:00 - 8:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="8:30 - 9:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="9:00 - 9:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="9:30 - 10:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="10:00 - 10:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="10:30 - 11:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="11:00 - 11:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="11:30 - 12:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="12:00 - 12:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="12:30 - 13:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="13:00 - 13:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="13:30 - 14:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="14:00 - 14:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="15:30 - 16:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="16:00 - 16:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="16:30 - 17:00" />
-
-//             </FormGroup>
-
-//             <FormGroup sx={{ px:5}}>
-//             <FormLabel component="legend">Friday</FormLabel>
-//             <FormControlLabel control={<Checkbox/>} label="8:00 - 8:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="8:30 - 9:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="9:00 - 9:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="9:30 - 10:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="10:00 - 10:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="10:30 - 11:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="11:00 - 11:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="11:30 - 12:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="12:00 - 12:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="12:30 - 13:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="13:00 - 13:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="13:30 - 14:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="14:00 - 14:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="15:30 - 16:00" />
-//                 <FormControlLabel control={<Checkbox/>} label="16:00 - 16:30"/>
-//                 <FormControlLabel control={<Checkbox/>} label="16:30 - 17:00" />
-
-//             </FormGroup>
-
-//             </Box>
-
-//             <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
-//           Submit
-//         </Button>
-
-//               </Paper>
-//             </div>
-//           );
-//         }
-  
-
