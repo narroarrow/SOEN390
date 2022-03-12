d1e6987bcd3815affe8055ef8baea95092f9a21d
Alexandru Bara <alexandru.bara.09@gmail.com>
2022-03-11 19:06:11 -0500

    Fixed format for displaying appointment slots

--- client/src/pages/PatientAppointment.js
+++ client/src/pages/PatientAppointment.js
@@ -4,8 +4,20 @@ import {Container, Box, FormLabel, RadioGroup, Radio, FormControl, FormControlLa
 import Axios from 'axios';
 
 function PatientAppointment() {
-  
-  //habdles the changes in radio button clicked
+
+  function getNextDayOfTheWeek(dayName, excludeToday = true, refDate = new Date()) {
+    const dayOfWeek = ["sun","mon","tue","wed","thu","fri","sat"]
+                      .indexOf(dayName.slice(0,3).toLowerCase());
+    if (dayOfWeek < 0) return;
+    refDate.setHours(0,0,0,0);
+    refDate.setDate(refDate.getDate() + +!!excludeToday + 
+                    (dayOfWeek + 7 - refDate.getDay() - +!!excludeToday) % 7);
+    return refDate;
+}
+
+console.log("Next is: " + getNextDayOfTheWeek("Wednesday", false));
+
+  //handles the changes in radio button clicked
   const [value, setValue] = React.useState('');
   const handleRadioChange = (event) => {
     setValue(event.target.value);
@@ -16,7 +28,7 @@ function PatientAppointment() {
     event.preventDefault();
     console.log(value);
     if(value != ''){
-      Axios.post('http://localhost:8080/PatientAppointment',{
+      Axios.post('http://localhost:8080/makeAppointments',{
         patientID: localStorage.getItem('id'),
         appointmentTime: value
       }, {withCredentials: true}).then(()=>{
--- server/app.js
+++ server/app.js
@@ -575,20 +575,38 @@ app.post("/validateDoctor", (req, res) => {
         }
     })
 });
-//
 
+//Finds the next day in the calenda
+function getNextDayOfTheWeek(dayName, excludeToday = true, refDate = new Date()) {
+    const dayOfWeek = ["sun","mon","tue","wed","thu","fri","sat"]
+                      .indexOf(dayName.slice(0,3).toLowerCase());
+    if (dayOfWeek < 0) return;
+    refDate.setHours(0,0,0,0);
+    refDate.setDate(refDate.getDate() + +!!excludeToday + 
+                    (dayOfWeek + 7 - refDate.getDay() - +!!excludeToday) % 7);
+    return refDate;
+}
+
+console.log("Next is: " + getNextDayOfTheWeek("Wednesday", false));
+
+//creates the array that is returned to Client
 function arrayMaker(result){
     // console.log(result)
     const returnedAvails = [];
     for(let i = 0; i < Object.keys(result).length; i++){
-        // console.log(`${result[i]["dayName"]} ${result[i]["StartTime"]} - ${result[i]["EndTime"]}`)
-        returnedAvails.push(`${result[i]["dayName"]} ${result[i]["StartTime"]} - ${result[i]["EndTime"]}`)
+        if(result[i]["dayName"] !=null){
+            //putting the appointment in the right format
+            // console.log()
+            // returnedAvails.push(`${result[i]["dayName"]} ${result[i]["StartTime"]} - ${result[i]["EndTime"]}`)
+            returnedAvails.push(""+getNextDayOfTheWeek(result[i]["dayName"], true).toString().slice(0, 15)+" "+result[i]["StartTime"]+" - "+ result[i]["EndTime"])
+        }
+
     }
     console.log(returnedAvails);
     return returnedAvails;
-
-    
 }
+
+
 //see open appointments
 app.get("/seeOpenAppointments", (req,res) => {
     //getting ID from client
