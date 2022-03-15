cbadbcc339a3b8ed8acce580b40b9a45c17a1c3a
giroux2000 <giroux2000@gmail.com>
2022-03-15 13:15:56 -0400

    test

--- client/src/__tests__/DoctorPatientProfile.test.js
+++ client/src/__tests__/DoctorPatientProfile.test.js
@@ -1,14 +1,15 @@
 import { render } from '@testing-library/react';
 import DoctorPatientProfile from '../pages/DoctorPatientProfile';
 import { BrowserRouter } from 'react-router-dom';
+import * as React from 'react';
 
 //This tests will pass if the DoctorPatientProfile page renders correctly
 test('Renders the DoctorPatientProfile page', () => {
 
-  localStorage.setItem('role', 'Doctor');
+    localStorage.setItem('role', 'Doctor');
 
-  render(
-    <BrowserRouter>
-      <DoctorPatientProfile />
-    </BrowserRouter>);
+    render(
+      <BrowserRouter>
+        <DoctorPatientProfile />
+      </BrowserRouter>);
 });
