ff305d17e22d866d66ae9e874dcded6adc6e81e8
Alexandru Bara <alexandru.bara.09@gmail.com>
2022-03-16 20:47:29 -0400

    Fixed merge conflicts

--- client/src/pages/DoctorViewingPatient.js
+++ client/src/pages/DoctorViewingPatient.js
@@ -110,6 +110,12 @@ function DoctorViewingPatient() {
         isFlagged = true;
     }
 
+    let isChatRequested = false; //variable to verify if the patient has requested to chat with the doctor.
+    let isChatRequestedArray = patientData.map((val, key) => {return val.ChatRequested});
+    if (isChatRequestedArray[0] === 1){ 
+        isChatRequested = true;
+    }
+
     let patientsDoctorID = patientData.map((val, key) => { return val.DoctorID })[0]; //retrieving the id of the patient's doctor
 
     let viewingDoctorsPatient = false; //variable to validate whether the doctor is viewing one of their own patients to restrict some actions.
@@ -206,8 +212,8 @@ function DoctorViewingPatient() {
                                         (<Button xs={12} sm={3} sx={{ margin: 1 }} variant="contained" onClick={markAsReviewed} disabled href='/DoctorViewingPatient'>MARK AS REVIEWED</Button>)}
                                     {/* This button will allow the doctor to accept a chat from a patient, initially  the chat is disabled.
                                     This action can only be performed by the patients own doctor. */}
-                                    {(isChatAccepted && viewingDoctorsPatient) ? (<Button xs={12} sm={3} sx={{ margin: 1 }} variant="contained" href='/DoctorViewingPatient'>ACCEPT CHAT</Button>) :
-                                        (<Button xs={12} sm={3} sx={{ margin: 1 }} variant="contained" disabled href='/DoctorViewingPatient' onClick={acceptChat}>ACCEPT CHAT</Button>)}
+                                    {(!isChatRequested || isChatAccepted || !viewingDoctorsPatient) ? (<Button xs={12} sm={3} sx={{ margin: 1 }} disabled variant="contained" href='/DoctorViewingPatient'>ACCEPT CHAT</Button>) :
+                                        (<Button xs={12} sm={3} sx={{ margin: 1 }} variant="contained"  href='/DoctorViewingPatient' onClick={acceptChat}>ACCEPT CHAT</Button>)}
                                 </Grid>
                             </Box>
                         </Box>
--- server/app.js
+++ server/app.js
@@ -108,7 +108,7 @@ app.get("/Viewed", (req, res) => {
 and display it in the UI. */
 app.get("/doctorViewingPatientData", (req, res) => {
     let pid = req.query.id;
-    db.query("SELECT U.Fname, U.Lname, P.ID, P.Status, P.NewPatient, Udoctor.Fname AS DoctorFirst, Udoctor.Lname AS DoctorLast, Udoctor.ID AS DoctorID, U.Email, U.Phone, U.Birthday, U.Address, P.SymptomRequested, P.ChatPermission, P.Flagged FROM 390db.patients P, 390db.users U, 390db.users Udoctor WHERE P.ID = ? AND P.ID = U.ID AND P.DoctorID = Udoctor.ID;", [pid], (err, result) => {
+    db.query("SELECT U.Fname, U.Lname, P.ID, P.Status, P.NewPatient, Udoctor.Fname AS DoctorFirst, Udoctor.Lname AS DoctorLast, Udoctor.ID AS DoctorID, U.Email, U.Phone, U.Birthday, U.Address, P.SymptomRequested, P.ChatPermission, P.Flagged, P.ChatRequested FROM 390db.patients P, 390db.users U, 390db.users Udoctor WHERE P.ID = ? AND P.ID = U.ID AND P.DoctorID = Udoctor.ID;", [pid], (err, result) => {
         if (err) {
             console.log(err);
         } else {
