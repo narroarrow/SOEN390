27c253b25422e3aa29f1fe1ad1ad435fbbccdec1
Jonathan Jong <60296341+infinatrix@users.noreply.github.com>
2022-03-29 12:57:13 -0400

    tests for ForgetPassword and PasswordReset added

---
+++ client/src/__tests__/PasswordReset.test.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import PasswordReset from '../pages/PasswordReset';
+
+test('Renders the Password Reset component', () => {
+  render(<PasswordReset />);
+});
