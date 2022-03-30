f6c1f1bed8c645043079c53ed2683946513a88f2
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2022-03-29 20:19:45 -0400

    Navbar fix name

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -48,7 +48,7 @@ const ResponsiveAppBar = () => {
             }
             if (localStorage.getItem('role') == 'Admin') {
                 pages.push(['AdminDashboard', 'Dashboard']);
-                pages.push(['AdminPatientDashboard', 'Patient Dashboard']);
+                pages.push(['AdminPatientDashboard', 'Patients']);
             }
             if (localStorage.getItem('role') == 'Doctor') {
                 pages.push(['DoctorDashboard', 'Dashboard']);
